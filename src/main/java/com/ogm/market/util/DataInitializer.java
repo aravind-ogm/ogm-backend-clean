2294f52037cc5bfe405be830adf2ac568ca18120
aravind-ogm <aravind.ogm@gmail.com>
2026-03-01 16:04:36 +0530

    url south1

--- src/main/java/com/ogm/market/util/DataInitializer.java
+++ src/main/java/com/ogm/market/util/DataInitializer.java
@@ -12,7 +12,7 @@ import java.util.Arrays;
 public class DataInitializer implements CommandLineRunner {
 
     private final PropertyRepository repo;
-    private static final String BASE_URL = "https://ogm-backend-clean-us-879813720468.us-central1.run.app";
+    private static final String BASE_URL = "https://ogm-backend-clean-879813720468.asia-south1.run.app";
 //    private static final String BASE_URL = "http://localhost:8080";
 
     public DataInitializer(PropertyRepository repo) {
