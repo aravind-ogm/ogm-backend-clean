0d1e3440b06c872833e9c22056925aa56b9a5a91
Aravindareddy Lingareddigari <aravindareddy.lingareddigari@gmail.com>
2026-04-02 14:58:38 +0530

    broker registration

--- src/main/java/com/ogm/market/PropertyBackendApplication.java
+++ src/main/java/com/ogm/market/PropertyBackendApplication.java
@@ -2,7 +2,9 @@ package com.ogm.market;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class PropertyBackendApplication {
     public static void main(String[] args) {
